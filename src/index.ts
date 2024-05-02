cfcb4bb0dcba59efffb1a9070a4a12e9e381a3db
Alexia <96691787+kitsunebunny@users.noreply.github.com>
2024-05-02 14:33:53 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import * as centra from "centra";
+import {
+  ApplicationCommand,
+  PartialApplicationCommand,
+  Snowflake,
+} from "./structures";
+
+import { verifySignature as _verifySignature } from "./utils";
+
+const DISCORD_ENDPOINT = "https://discord.com/api/v8/";
+
+const makeEndpoint = (endpoint) => `${DISCORD_ENDPOINT}${endpoint}`;
+
+export * from "./structures";
+export * from "./builders";
+
+export class DiscordInteractions {
+  private publicKey: Snowflake;
+  private applicationid: Snowflake;
+  private authToken: string;
+  private tokenPrefix: string;
+
+  constructor(opts: {
+    applicationId: Snowflake;
+    publicKey: Snowflake;
+    authToken: string;
+    tokenPrefix?: string;
+  }) {
+    this.publicKey = opts.publicKey;
+    this.applicationid = opts.applicationId;
+    this.authToken = opts.authToken;
+    this.tokenPrefix = (opts.tokenPrefix || "Bot") + " ";
+  }
+
+  verifySignature = (signature: string, timestamp: string, rawBody: string) => {
+    return _verifySignature(this.publicKey, signature, timestamp, rawBody);
+  };
+
+  getApplicationCommands = async (guildId?: string) => {
+    const request = await centra(
+      makeEndpoint(
+        guildId
+          ? `applications/${this.applicationid}/guilds/${guildId}/commands`
+          : `applications/${this.applicationid}/commands`
+      )
+    )
+      .header("Authorization", `${this.tokenPrefix}${this.authToken}`)
+      .send();
+    return (await request.json()) as ApplicationCommand[];
+  };
+
+  createApplicationCommand = async (
+    command: PartialApplicationCommand,
+    guildId?: string,
+    commandId?: string
+  ) => {
+    const suffix = commandId ? `/${commandId}` : "";
+    const method = commandId ? "PATCH" : "POST";
+    const request = await centra(
+      makeEndpoint(
+        guildId
+          ? `applications/${this.applicationid}/guilds/${guildId}/commands${suffix}`
+          : `applications/${this.applicationid}/commands${suffix}`
+      ),
+      method
+    )
+      .body(command, "json")
+      .header("Authorization", `${this.tokenPrefix}${this.authToken}`)
+      .send();
+    return (await request.json()) as ApplicationCommand;
+  };
+
+  editApplicationCommand = async (
+    commandId: string,
+    command: PartialApplicationCommand,
+    guildId?: string
+  ) => {
+    return await this.createApplicationCommand(command, guildId, commandId);
+  };
+
+  deleteApplicationCommand = async (commandId: string, guildId?: string) => {
+    const request = await centra(
+      makeEndpoint(
+        guildId
+          ? `applications/${this.applicationid}/guilds/${guildId}/commands/${commandId}`
+          : `applications/${this.applicationid}/commands/${commandId}`
+      ),
+      "DELETE"
+    )
+      .header("Authorization", `${this.tokenPrefix}${this.authToken}`)
+      .send();
+    return request.statusCode == 204;
+  };
+}
+export default DiscordInteractions;
